e45ab8a6c9f86e94a8fa4e51050b4f727918da82
Nina Zhou <nzhou@lcs.on.ca>
2024-01-11 18:51:20 -0500

    Initial Commit

---
+++ AppleTutorial/AppleTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTutorialApp.swift
+//  AppleTutorial
+//
+//  Created by Shuyu Zhou on 2024-01-11.
+//
+
+import SwiftUI
+
+@main
+struct AppleTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
