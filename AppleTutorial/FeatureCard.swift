b87b2d6d830407f1cba086ff8ebd4fdae777dd08
Nina Zhou <nzhou@lcs.on.ca>
2024-01-11 19:33:19 -0500

    added the feature card helper view

---
+++ AppleTutorial/FeatureCard.swift
@@ -0,0 +1,28 @@
+//
+//  FeatureCard.swift
+//  AppleTutorial
+//
+//  Created by Shuyu Zhou on 2024-01-11.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    let iconName : String
+    let description : String
+    
+    var body: some View {
+        HStack{
+            Image(systemName: iconName)
+                .font(.largeTitle)
+            Text(description)
+        }
+        .padding()
+        .background(.tint, in: RoundedRectangle(cornerRadius: 12))
+        .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    FeatureCard(iconName: "figure.2.and.child.holdinghands", description: "something")
+}
---
+++ AppleTutorial/FeaturesPage.swift
@@ -0,0 +1,23 @@
+//
+//  FeaturesPage.swift
+//  AppleTutorial
+//
+//  Created by Shuyu Zhou on 2024-01-11.
+//
+
+import SwiftUI
+
+struct FeaturesPage: View {
+    var body: some View {
+        VStack {
+            Text("Features")
+                .font(.title)
+            .fontWeight(.semibold)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FeaturesPage()
+}
